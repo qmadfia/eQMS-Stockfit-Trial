062ac6d56286f81e5ee8de5ea9f59aa1d2f6ce6f
qmadfia <qm.adfia@gmail.com>
2025-08-21 12:55:55 +0700

    Update scale.js

--- scale.js
+++ scale.js
@@ -1,14 +1,14 @@
 function scaleApp() {
   const app = document.querySelector('.app');
-  const scaleX = window.innerWidth / 1920;
-  const scaleY = window.innerHeight / 1080;
+  const scaleX = window.innerWidth / 2237;
+  const scaleY = window.innerHeight / 1150;
   const scale = Math.min(scaleX, scaleY); // biar pas tanpa scroll
   app.style.transform = `scale(${scale})`;
 
   // Centering kalau layar lebih lebar/tinggi
   app.style.position = "absolute";
-  app.style.left = `${(window.innerWidth - 1920 * scale) / 2}px`;
-  app.style.top = `${(window.innerHeight - 1080 * scale) / 2}px`;
+  app.style.left = `${(window.innerWidth - 2237 * scale) / 2}px`;
+  app.style.top = `${(window.innerHeight - 1150 * scale) / 2}px`;
 }
 
 window.addEventListener('resize', scaleApp);
