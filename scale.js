68427411408ba87bbf1f319ed6560ed72e0214b5
qmadfia <qm.adfia@gmail.com>
2025-08-21 13:09:06 +0700

    Create scale.js

---
+++ scale.js
@@ -0,0 +1,15 @@
+function scaleApp() {
+  const app = document.querySelector('.app');
+  const scaleX = window.innerWidth / 2237;
+  const scaleY = window.innerHeight / 1080;
+  const scale = Math.min(scaleX, scaleY); // biar pas tanpa scroll
+  app.style.transform = `scale(${scale})`;
+
+  // Centering kalau layar lebih lebar/tinggi
+  app.style.position = "absolute";
+  app.style.left = `${(window.innerWidth - 2237 * scale) / 2}px`;
+  app.style.top = `${(window.innerHeight - 1080 * scale) / 2}px`;
+}
+
+window.addEventListener('resize', scaleApp);
+window.addEventListener('load', scaleApp);
